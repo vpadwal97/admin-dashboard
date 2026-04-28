35778187274ca21e9336921dc1ad71b39d8ce8e2
Vishu <h.ara.devi.e.r@gmail.com>
2026-04-28 11:47:21 +0000

    done

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["6v5jwk-5173.csb.app"],
+    // allowedHosts: ["6v5jwk-5173.csb.app", "cpljxg-5173.csb.app"],
+    allowedHosts: true,
   },
 });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     allowedHosts: ["6v5jwk-5173.csb.app"]
-//   }
-// });
